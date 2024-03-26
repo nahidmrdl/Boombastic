e7b4cc9c39670fb7f5675feffd75c083d5a6e406
nahidmrdl <muradlinahid2003@gmail.com>
2024-03-27 00:49:37 +0100

    Modified some gui elements

--- game/src/gui/GameTopPanelGUI.java
+++ game/src/gui/GameTopPanelGUI.java
@@ -5,9 +5,9 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameTopPanelGUI extends JPanel {
-    // Constructor and any additional methods
+
     public GameTopPanelGUI() {
         super();
-        // Initialize your panel here
+
     }
 }
