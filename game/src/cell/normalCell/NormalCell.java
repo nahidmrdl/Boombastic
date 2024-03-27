1b13514daa62fc5900d78bd08176f688fab94d54
cdwcrz <cdwcrz@inf.elte.hu>
2024-03-27 01:17:24 +0100

    modify Cell and child classes

--- game/src/cell/normalCell/NormalCell.java
+++ game/src/cell/normalCell/NormalCell.java
@@ -1,6 +1,14 @@
 package cell.normalCell;
 
 import cell.Cell;
+import entity.player.Player;
+
+import java.awt.*;
 
 public class NormalCell extends Cell {
+    private Image image;
+    public NormalCell(int row, int col, String type, Player owner, Image image) {
+        super(row, col, type, owner);
+        this.image = image;
+    }
 }
