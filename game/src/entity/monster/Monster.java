b9b6cab9ccfc54f57176cacab6d943f20c7de2bb
cdwcrz <cdwcrz@inf.elte.hu>
2024-03-27 21:03:04 +0100

    Modifications to Entity, monster, player, gameItem.

--- game/src/entity/monster/Monster.java
+++ game/src/entity/monster/Monster.java
@@ -1,6 +1,19 @@
 package entity.monster;
 
 import entity.Entity;
+import map.Map;
+
+import java.awt.*;
 
 public class Monster extends Entity {
+
+    private Image baseImage;
+    private String direction;
+    private int speed;
+
+    public Monster(int x, int y, Map map, int speed) {
+        super(x, y, map);
+        this.speed = speed;
+
+    }
 }
--- game/src/entity/player/Player.java
+++ game/src/entity/player/Player.java
@@ -1,18 +1,22 @@
 package entity.player;
 
 import entity.Entity;
+import map.Map;
 
 import java.util.HashMap;
 
 public class Player extends Entity {
     private String name;
     private int imageIndex;
+
+    public Player(int x, int y, Map map, String name, int imageIndex) {
+        super(x, y, map);
+        this.imageIndex = imageIndex;
+
+    }
     //private HashMap<String, String> Controls;
 
-    public Player(String name, int imageIndex) {
-        this.name = name;
-        this.imageIndex = imageIndex;
-    }
+
 
     // getters and setters
     public String getName() {
