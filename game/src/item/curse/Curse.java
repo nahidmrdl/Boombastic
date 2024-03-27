eb994531e6c5cf65efbaf9b2135267459136ef85
nahidmrdl <muradlinahid2003@gmail.com>
2024-03-27 21:33:13 +0100

    Added GameItem abstract class

--- game/src/item/curse/Curse.java
+++ game/src/item/curse/Curse.java
@@ -3,4 +3,11 @@ package item.curse;
 import item.GameItem;
 
 public abstract class Curse extends GameItem {
+    public Curse(Image baseImage) {
+        super(baseImage);
+    }
+
+    public abstract void apply(Player p);
+
+    public abstract void reset(Player p);
 }
--- game/src/item/powerup/PowerUp.java
+++ game/src/item/powerup/PowerUp.java
@@ -2,5 +2,13 @@ package item.powerup;
 
 import item.GameItem;
 
+
 public abstract class PowerUp extends GameItem {
+    public PowerUp(Image baseImage) {
+        super(baseImage);
+    }
+
+    public abstract void apply(Player p);
+
+    public abstract void reset(Player p);
 }
