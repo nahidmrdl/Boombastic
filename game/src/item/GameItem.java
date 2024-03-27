6592ba66853c4ac0e4467e0b14683f92500ab3de
nahidmrdl <muradlinahid2003@gmail.com>
2024-03-27 20:44:11 +0100

    Added GameItem abstract class

--- game/src/item/GameItem.java
+++ game/src/item/GameItem.java
@@ -1,4 +1,28 @@
 package item;
 
 public abstract class GameItem {
+    private Image baseImage;
+    private Image image;
+    private long finishTime;
+    private Cell cell;
+
+    public void setCell(Cell c){
+        this.cell = c;
+    }
+
+    public Cell getCell(){
+        return this.cell;
+    }
+
+    public long getFinishTime(){
+        return this.finishTime;
+    }
+
+    public Image getBaseImage(){
+        return this.baseImage;
+    }
+
+    public Image getImage(){
+        return this.image;
+    }
 }
