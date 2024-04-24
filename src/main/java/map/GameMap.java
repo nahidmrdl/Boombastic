c02a43f963cd289d1ac914ca88d37c203fbb554f
Muradli Nahid <hjpivf@inf.elte.hu>
2024-04-24 22:58:53 +0200

    Added documentation for GameMap and ResourceCollection

--- src/main/java/map/GameMap.java
+++ src/main/java/map/GameMap.java
@@ -12,11 +12,20 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a game map.
+ */
 public class GameMap {
     private Cell[][] map;
     private Image image;
     private String name;
 
+    /**
+     * Constructor for GameMap
+     * @param image image of the map
+     * @param name name of the map
+     * @param mapIndex index of the map
+     */
     public GameMap(Image image, String name, Integer mapIndex){
         try {
             //(STR."src/levels/\{mapIndex}.txt");
@@ -38,14 +47,28 @@ public class GameMap {
 
     }
 
+    /**
+     * Returns the map
+     * @return the map
+     */
     public Cell[][] getMap(){
         return this.map;
     }
 
+    /**
+     * Returns the cell at the given coordinates
+     * @param x x-coordinate
+     * @param y y-coordinate
+     * @return the cell at the given coordinates
+     */
     public Cell getCell(int x, int y){
         return this.map[x][y];
     }
 
+    /**
+     * Detonates all bombs of the player
+     * @param player
+     */
     public void DetonatePlayerBombs(Player player) {
         for (Cell[] cells : map) {
             for (Cell cell : cells) {
@@ -63,10 +86,18 @@ public class GameMap {
         }
     }
 
+    /**
+     * Returns the image of the map
+     * @return the image of the map
+     */
     public Image getImage(){
         return this.image;
     }
 
+    /**
+     * Returns the name of the map
+     * @return the name of the map
+     */
     public String getName(){
         return this.name;
     }
