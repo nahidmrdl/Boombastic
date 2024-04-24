46c96435a659d8586c37df47bd6af6a805c27260
Muradli Nahid <hjpivf@inf.elte.hu>
2024-04-24 22:22:09 +0200

    Added documentation to Box, Normal and Wall cells

--- src/main/java/cell/box/BoxCell.java
+++ src/main/java/cell/box/BoxCell.java
@@ -12,19 +12,35 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Random;
 
+/**
+ * Represents a box cell in the game map.
+ */
 public class BoxCell extends Cell {
     private Image powerUpImage;
     private Player owner;
 
+    /**
+     * Constructor for BoxCell
+     * @param row row of the cell
+     * @param col column of the cell
+     * @param map map that the cell is in
+     */
     public BoxCell(int row, int col, GameMap map) {
         super(row, col, map);
         this.image = ResourceCollection.Images.PLACED_BOX.getImage();
         this.owner = null;
     }
 
+    /**
+     * Sets an owner to the box
+     */
     public void setOwner(Player owner) {
         this.owner = owner;
     }
+
+    /**
+     * Gets the owner of the box
+     */
     public Player getOwner() {
         return this.owner;
     }
