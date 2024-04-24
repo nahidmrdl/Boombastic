a21e1a594088f427ebeada7f0978f9f2d21a2e75
Muradli Nahid <hjpivf@inf.elte.hu>
2024-04-24 22:56:11 +0200

    Added documentation for PowerUp and all powerup types

--- src/main/java/item/powerup/PowerUp.java
+++ src/main/java/item/powerup/PowerUp.java
@@ -5,10 +5,15 @@ import item.GameItem;
 import util.ResourceCollection;
 
 import java.awt.*;
-
-
+/**
+ * Represents a power-up in the game map.
+ */
 public abstract class PowerUp extends GameItem {
 
+    /**
+     * Constructor for PowerUp
+     * @param image image of the power-up
+     */
     public PowerUp(Image image) {
         super(image);
         this.baseImage = ResourceCollection.Images.POWERUP_ICON.getImage();
@@ -19,7 +24,16 @@ public abstract class PowerUp extends GameItem {
         return System.currentTimeMillis() > getFinishTime() - 3000 && getFinishTime() != 0;
     }
 
+    /**
+     * Applies the power-up
+     * @param p player to apply the power-up to
+     */
     public abstract void apply(Player p);
 
+    /**
+     * Resets the power-up
+     * @param p player to reset the power-up for
+     */
     public abstract void reset(Player p);
 }
+
--- src/main/java/item/powerup/placeobstacle/PlaceObstacle.java
+++ src/main/java/item/powerup/placeobstacle/PlaceObstacle.java
@@ -4,17 +4,31 @@ import entity.player.Player;
 import item.powerup.PowerUp;
 import util.ResourceCollection;
 
+/**
+ * Represents a power-up that allows the player to place obstacles for some time.
+ */
 public class PlaceObstacle extends PowerUp {
 
+        /**
+         * Constructor for PlaceObstacle
+         */
         public PlaceObstacle() {
             super(ResourceCollection.Images.OBSTACLE_POWERUP.getImage());
         }
 
+        /**
+         * Applies the power-up to the player
+         * @param p player to apply the power-up to
+         */
         @Override
         public void apply(Player p) {
             p.setPlaceObsticleCount(p.getPlaceObsticleCount() + 3);
         }
 
+        /**
+         * Resets the power-up for the player
+         * @param p player to reset the power-up for
+         */
         @Override
         public void reset(Player p) {
 
