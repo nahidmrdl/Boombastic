c55e8962266af7828b40fc0fb5e4831e80253a93
Muradli Nahid <hjpivf@inf.elte.hu>
2024-04-24 22:52:14 +0200

    Added documentation for Curse and all curse types

--- src/main/java/item/curse/bombblastreduction/BombBlastReduction.java
+++ src/main/java/item/curse/bombblastreduction/BombBlastReduction.java
@@ -5,13 +5,22 @@ import item.curse.Curse;
 import util.ResourceCollection;
 
 import java.awt.*;
-
+/**
+ * Represents a curse that reduces the bomb blast range of the player for some time.
+ */
 public class BombBlastReduction extends Curse {
 
+    /**
+     * Constructor for BombBlastReduction
+     */
     public BombBlastReduction() {
         super(ResourceCollection.Images.LOWBLASTRANGE_CURSE.getImage());
     }
 
+    /**
+     * Applies the curse to the player
+     * @param p player to apply the curse to
+     */
     @Override
     public void apply(Player p) {
         p.setBombBlastRange(0);
@@ -19,6 +28,10 @@ public class BombBlastReduction extends Curse {
         setFinishTime(System.currentTimeMillis() + 5000);
     }
 
+    /**
+     * Resets the curse for the player
+     * @param p player to reset the curse for
+     */
     @Override
     public void reset(Player p) {
         p.setBombBlastRange(1);
