eb5d8f3634bea9ac697c717516b62fda3886edc5
nahidmrdl <muradlinahid2003@gmail.com>
2024-05-15 23:22:05 +0200

    Deleted a comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 import gui.GameGUI;
 import util.ResourceCollection;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-//Main File
 public class Main {
     public static void main(String[] args) {
         ResourceCollection.readFiles();
